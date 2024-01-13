ca0b9486ca0f0dc647916b24803bf72b9593d475
Vít <xvodick6@mendelu.cz>
2024-01-13 16:56:43 +0100

    Simplification, data for boxes, dice

--- src/main/java/com/example/javafxproject1/HelloApplication.java
+++ src/main/java/com/example/javafxproject1/HelloApplication.java
@@ -13,6 +13,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        scene.getStylesheets().add(getClass().getResource("/css/styles.css").toExternalForm());
         stage.setTitle("Cashflow");
         stage.setScene(scene);
         stage.setMaximized(true);
--- src/main/java/com/example/javafxproject1/HelloController.java
+++ src/main/java/com/example/javafxproject1/HelloController.java
@@ -1,14 +1,60 @@
 package com.example.javafxproject1;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Screen;
+
+import java.io.File;
+import java.util.Random;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+    Random random = new Random();
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ImageView diceImage;
+
+    @FXML
+    private Button rollButton;
+
+    @FXML
+    public void initialize() {
+        double screenWidth = Screen.getPrimary().getVisualBounds().getWidth();
+        diceImage.setLayoutX(screenWidth-250);
+        diceImage.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                // Call the method you want to execute on ImageView click
+                roll();
+            }
+        });
+    }
+
+    @FXML
+    void roll() {
+        diceImage.setDisable(true);
+        Thread thread = new Thread(){
+            public void run(){
+                System.out.println("Thread Running");
+                try {
+                    for (int i = 0; i < 15; i++) {
+                        File file = new File("src/main/resources/dice-" + (random.nextInt(6)+1)+".png");
+                        diceImage.setImage(new Image(file.toURI().toString()));
+                        Thread.sleep(50);
+                        diceImage.setDisable(false);
+                    }
+                    rollButton.setDisable(false);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        thread.start();
     }
 }
